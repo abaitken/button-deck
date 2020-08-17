af3005dc30270faf41d74d6e18ffc784828707c5
Alex Boyne-Aitken <abaitken@live.co.uk>
2020-08-17 23:42:10 +0100

    Created project and referenced obs websocket for obs communication

---
+++ Client/ButtonDeckClient.OBS/Class1.cs
@@ -0,0 +1,19 @@
+﻿using OBS.WebSocket.NET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ButtonDeckClient.OBS
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            var obs = new ObsWebSocket();
+            var obsApi = new ObsWebSocketApi(obs);
+            // TODO 
+        }
+    }
+}
