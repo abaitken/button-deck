fba77dddb6bd370590ad6b14a02c3d5789548103
Alex Boyne-Aitken <abaitken@live.co.uk>
2020-09-08 11:01:25 +0100

    Added some commands, playing with some ideas

---
+++ Client/ButtonDeckClient/Integrations/BuiltIn/Delay.cs
@@ -0,0 +1,23 @@
+﻿using ButtonDeckClient.Workflows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ButtonDeckClient.Integrations.BuiltIn
+{
+    public class Delay : Command
+    {
+        public int Time { get; set; }
+
+        public override void Execute()
+        {
+            if (Time <= 0)
+                return;
+
+            Thread.Sleep(Time);
+        }
+    }
+}
---
+++ Client/ButtonDeckClient/Integrations/OBS/StartRecording.cs
@@ -0,0 +1,15 @@
+﻿using ButtonDeckClient.Workflows;
+using OBS.WebSocket.NET;
+
+namespace ButtonDeckClient.Integrations.OBS
+{
+    public class StartRecording : Command
+    {
+        public ObsWebSocketApi OBS { get; set; }
+
+        public override void Execute()
+        {
+            OBS.StartRecording();
+        }
+    }
+}
